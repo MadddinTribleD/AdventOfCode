eb2d6cca27a36b6c7d26129ad3706f275e3c8fb2
MadddinTribleD <martin@gafert.org>
2024-12-14 01:17:28 +0100

    Draft: Add 2024 day 13 part 2

--- 2024/day13/main.go
+++ 2024/day13/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -47,8 +48,8 @@ func NewClawMachine(match []string) ClawMachine {
 		AY: values[1],
 		BX: values[2],
 		BY: values[3],
-		X:  values[4],
-		Y:  values[5],
+		X:  values[4] + 10000000000000,
+		Y:  values[5] + 10000000000000,
 	}
 }
 
@@ -73,47 +74,88 @@ func main() {
 
 	sum := int64(0)
 
-	for _, clawMachine := range clawMachines {
-		results := []ButtonPresses{}
+	for i, cl := range clawMachines {
+		// part1Result := part1(cl)
 
-		for a := int64(1); a <= 100; a++ {
+		sx := (float64(-cl.X)*(float64(cl.BY)/float64(cl.BX)) + float64(cl.Y)) /
+			((float64(cl.AY) / float64(cl.AX)) - (float64(cl.BY) / float64(cl.BX)))
 
-			ax := clawMachine.AX * a
-			ay := clawMachine.AY * a
+		tokens := int64(0)
 
-			if ax > clawMachine.X || ay > clawMachine.Y {
-				break
-			}
-
-			for b := int64(1); b <= 100; b++ {
-				x := ax + b*clawMachine.BX
-
-				if clawMachine.X == x {
-					y := ay + b*clawMachine.BY
-					if clawMachine.Y == y {
-						results = append(results, ButtonPresses{A: a, B: b})
-					} else if clawMachine.Y < y {
-						break
-					}
-				} else if clawMachine.X < x {
-					break
-				}
-			}
+		if i == 42 {
+			fmt.Print("")
 		}
 
-		if len(results) > 0 {
-			tokens := results[0].Tokens()
+		if IsInt(sx) {
+			a := int64(math.Round(sx / float64(cl.AX)))
+			b := int64(math.Round((float64(cl.X) - sx) / float64(cl.BX)))
 
-			for i := 1; i < len(results); i++ {
-				t := results[i].Tokens()
-				if t < tokens {
-					tokens = t
-				}
-			}
-
-			sum += tokens
+			tokens = a*3 + b
 		}
+
+		// if part1Result != tokens {
+		// 	fmt.Printf("i=%d expected token: %d, got %d\n", i, part1Result, tokens)
+		// }
+
+		sum += tokens
 	}
 
 	fmt.Printf("Minimum Tokens: %d", sum)
 }
+
+func IsInt(f float64) bool {
+	intFloat := math.Round(f)
+
+	if f == intFloat {
+		return true
+	}
+
+	const e = 0.0001
+	isInt := f+e > intFloat && f-e < intFloat
+
+	return isInt
+}
+
+// func part1(clawMachine ClawMachine) int64 {
+// 	results := []ButtonPresses{}
+
+// 	for a := int64(1); a <= 100; a++ {
+
+// 		ax := clawMachine.AX * a
+// 		ay := clawMachine.AY * a
+
+// 		if ax > clawMachine.X || ay > clawMachine.Y {
+// 			break
+// 		}
+
+// 		for b := int64(1); b <= 100; b++ {
+// 			x := ax + b*clawMachine.BX
+
+// 			if clawMachine.X == x {
+// 				y := ay + b*clawMachine.BY
+// 				if clawMachine.Y == y {
+// 					results = append(results, ButtonPresses{A: a, B: b})
+// 				} else if clawMachine.Y < y {
+// 					break
+// 				}
+// 			} else if clawMachine.X < x {
+// 				break
+// 			}
+// 		}
+// 	}
+
+// 	if len(results) > 0 {
+// 		tokens := results[0].Tokens()
+
+// 		for i := 1; i < len(results); i++ {
+// 			t := results[i].Tokens()
+// 			if t < tokens {
+// 				tokens = t
+// 			}
+// 		}
+
+// 		return tokens
+// 	}
+
+// 	return 0
+// }
