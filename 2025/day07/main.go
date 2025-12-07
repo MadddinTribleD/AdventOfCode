b3cf8832aa2a8376a75c75d05da97a6b32ed9e56
MadddinTribleD <martin@gafert.org>
2025-12-07 21:44:30 +0100

    Add 2025 day 7 part 2

--- 2025/day07/main.go
+++ 2025/day07/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 const (
@@ -13,6 +14,8 @@ const (
 	Void     byte = '.'
 )
 
+var allVoidRegExp = regexp.MustCompile(`^\.*$`)
+
 func main() {
 	data, err := os.ReadFile("input")
 	if err != nil {
@@ -20,38 +23,57 @@ func main() {
 	}
 	lines := bytes.Split(data, []byte("\n"))
 
+	splits := make([]map[int]int, 0, len(lines))
 	for i := 0; i < len(lines[0]); i++ {
 		if lines[0][i] == Start {
 			lines[0][i] = Beam
+
+			splits = append(splits, map[int]int{
+				i: 1,
+			})
+			break
 		}
 	}
 
-	splits := 0
+	for i := len(lines) - 1; i >= 0; i-- {
+		if allVoidRegExp.Match(lines[i]) {
+			lines = append(lines[:i], lines[i+1:]...)
+		}
+	}
 
 	for i := 1; i < len(lines); i++ {
+		splits = append(splits, map[int]int{})
 		for j := 0; j < len(lines[i]); j++ {
 			isSplitter := lines[i][j] == Splitter
 
+			previous := splits[i-1][j]
 			if lines[i-1][j] == Beam {
-
 				if isSplitter {
-					if j > 0 && lines[i][j-1] == Void {
+					if j > 0 && lines[i][j-1] != Splitter {
 						lines[i][j-1] = Beam
+						splits[i][j-1] += previous
 					}
-					if j < len(lines[i])-1 && lines[i][j+1] == Void {
+					if j < len(lines[i])-1 && lines[i][j+1] != Splitter {
 						lines[i][j+1] = Beam
+						splits[i][j+1] += previous
 					}
-					splits++
+
 				} else {
 					lines[i][j] = Beam
+					splits[i][j] += previous
 				}
 			}
 		}
+	}
+	print(lines)
 
-		print(lines)
+	timelines := 0
+
+	for _, v := range splits[len(splits)-1] {
+		timelines += v
 	}
 
-	fmt.Printf("Tachyons splits: %d\n", splits)
+	fmt.Printf("Total timelines: %d\n", timelines)
 }
 
 func print(lines [][]byte) {
